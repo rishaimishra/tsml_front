530bfba7fffc5705ac1467039d58a989a21831a3
Arun Sarkar <113158551+arun28sarkar@users.noreply.github.com>
2022-11-21 18:53:39 +0530

    index and prod file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-
+  
+  // apiEndpointBase: 'https://beas.in/mje-shop/api',
   // apiEndpointBase: 'https://demo-tsml.mjunction.in/tsml-api/api',
 
 
